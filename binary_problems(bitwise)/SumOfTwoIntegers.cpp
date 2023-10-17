09dbe851e925b9f79b9bfb02be876f3dea497821
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-10-17 13:28:03 -0700

    Added Solution for Sum of Two Integers
    Implemented a class called Solution.

--- binary_problems(bitwise)/SumOfTwoIntegers.cpp
+++ binary_problems(bitwise)/SumOfTwoIntegers.cpp
@@ -14,12 +14,15 @@
 
   LeetCode Link: https://leetcode.com/problems/sum-of-two-integers/
 */
-int getSum(int a, int b) {
-  while (b != 0) {
-    int carry = a & b;  // Calculate the carry.
-    a = a ^ b;  // Sum without carry.
-    b = carry << 1;  // Shift the carry to the left.
+class Solution {
+public:
+  int getSum(int a, int b) {
+    while (b != 0) {
+      int carry = a & b;  // Calculate the carry.
+      a = a ^ b;  // Sum without carry.
+      b = carry << 1;  // Shift the carry to the left.
+    }
+  
+    return a;
   }
-
-  return a;
 }
